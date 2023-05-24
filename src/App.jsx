e83a24d6a3f856fe96146e5b06db66367a1a5bde
Elharfali <elharfali.youssef.20@gmail.com>
2023-05-24 19:13:43 +0100

    Add and Remove Tasks

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+import Form from "./components/Form"
+import { nanoid } from "nanoid"
+import { ToastContainer, toast } from "react-toastify";
+import {AiFillDelete} from 'react-icons/ai'
+
+
+function App() {
+  const [tasks, setTasks] = useState([])
+
+  console.log(tasks);
+
+  const addNewTask = (taskName) => {
+    const newTask = {
+      name: taskName,
+      id: nanoid(),
+      completed: false
+    }
+    setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    toast.success('The Task Is Removed')
+    setTasks(newTasks)
+  }
+
+  return (
+    <main className="h-[100vh] w-full bg-gradient-to-r from-cyan-500 to-blue-500 py-10 px-4">
+      <div className=" max-w-[800px] p-6 bg-white mx-auto rounded-xl shadow-md shadow-cyan-900">
+        <div className="w-full flex flex-col items-center justify-center mb-6">
+          <h1 className="text-2xl md:text-3xl font-semibold pb-1">Todo-List</h1>
+          <div className="w-28 rounded-xl  h-1 bg-sky-500" />
+        </div>
+        <Form addNewTask={addNewTask} tasks={tasks} />
+        <div className="my-4">
+          {tasks.map((task) => {
+            const { name, id, completed } = task
+            return (
+              <article key={id} className="p-2 bg-[#91D8E4] rounded-md flex items-center justify-between mb-2">
+                <div className="flex items-center gap-5">
+                  <input type="checkbox" checked={completed} />
+                  <h3 className="text-lg md:text-xl font-medium capitalize">{name}</h3>
+                </div>
+                <button type="button" onClick={() => deleteTask(id)} className="bg-red-400 text-white p-1 rounded-md transition-all ease-in-out duration-150 hover:bg-red-600">
+                  <AiFillDelete size={25} />
+                </button>
+              </article>
+            )
+          })}
+        </div>
+        <ToastContainer position="top-center" />
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,44 @@
+import { useRef, useEffect, useState } from "react"
+import { AiOutlineSearch } from 'react-icons/ai'
+
+import {toast} from 'react-toastify'
+
+const Form = ({addNewTask, tasks}) => {
+    const inputRef = useRef(null)
+    const [inputValue, setInputValue] = useState('')
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (inputValue) {
+            if (tasks.find((task) => task.name === inputValue)) {
+                toast.warning('Task Already Exists!')
+                return;
+            }
+            toast.success('The task has been added successfully')
+            addNewTask(inputValue)
+        }
+        
+        return;
+    }
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+  return (
+      <form onSubmit={handleSubmit} className='w-full h-full'>
+          <div className='flex h-full w-full'>
+              <input type="text" ref={inputRef} onChange={handleChange} className='w-full   rounded-tl-md rounded-bl-md focus:outline-none px-2 py-1 bg-sky-200 text-xl' placeholder="Add a Task..."  />
+              <button type='submit' className='py-2 px-4 bg-blue-600 transition-all ease-in-out duration-150 hover:bg-blue-500 rounded-tr-md rounded-br-md'>
+                  <AiOutlineSearch size={25} />
+              </button>
+          </div>
+    </form>
+  )
+}
+
+export default Form 
